63d73ed2b525375ecd04fc595f26eb1ab5b0a4bd
Shorno <fb.shorno@gmail.com>
2025-12-14 01:09:17 +0600

    generate schema

--- src/db/config/index.ts
+++ src/db/config/index.ts
@@ -1,6 +1,19 @@
-import {Pool} from '@neondatabase/serverless';
-import {drizzle} from 'drizzle-orm/neon-serverless';
+// import {Pool} from '@neondatabase/serverless';
+// import {drizzle} from 'drizzle-orm/neon-serverless';
+// import * as schema from "../schema/index.js"
+//
+// const pool = new Pool({connectionString: process.env.DATABASE_URL!})
+// export const db = drizzle({client: pool, schema})
 import * as schema from "../schema/index.js"
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import ws from "ws";
 
-const pool = new Pool({connectionString: process.env.DATABASE_URL!})
-export const db = drizzle({client: pool, schema})
+neonConfig.webSocketConstructor = ws;
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true
+
+const sql = neon(process.env.DATABASE_URL || "");
+export const db = drizzle(sql, { schema });
+
